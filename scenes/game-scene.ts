e863095ba34a13d28542122bcd5ab9e4fb2ca6d5
Eugene Gorelenko <oldtoy480@gmail.com>
2022-05-25 14:23:58 +0300

    Update generate map

--- scenes/game-scene.ts
+++ scenes/game-scene.ts
@@ -4,74 +4,14 @@ import EasyStar from 'easystarjs';
 import tilemapPng from '../assets/tileset/Green_Meadow_Tileset.png';
 import tilemapJson from '../assets/green_meadow.json';
 import CharacterFactory, {
-	BuildSlimeOptions,
 } from '../src/characters/character_factory';
 import { Scene } from '../src/characters/scene';
-import { Arbitrator } from '../src/ai/behaviour/arbitrator';
 import Vector from '../src/utils/vector';
 
 type LayerDescription = {
 	depth?: number;
 	collide?: boolean;
 };
-
-const layersSettings = {
-	/** Нижний слой с землёй и декором */
-	Ground: {} as LayerDescription,
-	/** Стены, вода */
-	Walls: { collide: true } as LayerDescription,
-	/** Одиночные препятствия (камни, пни) */
-	Obstacles: { collide: true } as LayerDescription,
-	/** Стены загона  - возможно, отдельный слой не нужен */
-	'Corral.Walls': { collide: true } as LayerDescription,
-	/** Двери загона  - возможно, отдельный слой не нужен */
-	'Corral.Doors': {} as LayerDescription,
-	/** Арбитр в загоне - возможно, отдельный слой не нужен */
-	'Corral.Arbitrator': { depth: 10 } as LayerDescription,
-	/** Арбитр снаружи - возможно, отдельный слой не нужен */
-	Arbitrator: { depth: 10 } as LayerDescription,
-	/** Декор сверху (кроны деревьев) */
-	Transparent: { depth: 10 } as LayerDescription,
-};
-
-/**
- * Вспомогательная функция для создания слоёв по описанию в виде объекта,
- * потому что мне надоело хардкодить-копипастить
- */
-function createLayers<T extends string>(
-	map: Phaser.Tilemaps.Tilemap,
-	tileset: Phaser.Tilemaps.Tileset,
-	layersSettings: Record<T, LayerDescription>
-): Record<T, Phaser.Tilemaps.TilemapLayer> {
-	const layers = {} as Record<T, Phaser.Tilemaps.TilemapLayer>;
-	for (const layerID in layersSettings) {
-		layers[layerID] = map.createLayer(layerID, tileset, 0, 0);
-		const depth = layersSettings[layerID].depth;
-		if (depth !== undefined) layers[layerID].setDepth(depth);
-	}
-	return layers;
-}
-
-function setupFinder(
-	finder: EasyStar.js,
-	tilemap: Phaser.Tilemaps.Tilemap,
-	collidesLayers: string[]
-): void {
-	const grid = [];
-	for (let y = 0; y < tilemap.height; y++) {
-		const col = [];
-		for (let x = 0; x < tilemap.width; x++) {
-			const tile = collidesLayers.reduce(
-				(tile, layer) => tile || tilemap.getTileAt(x, y, false, layer)?.index,
-				null as number | null
-			);
-			col.push(tile ? tile : 0);
-		}
-		grid.push(col);
-	}
-	finder.setGrid(grid);
-	finder.setAcceptableTiles([0]);
-}
 
 export class GameScene extends Phaser.Scene implements Scene {
 	public readonly finder = new EasyStar.js();
@@ -94,16 +34,104 @@ export class GameScene extends Phaser.Scene implements Scene {
 		this.load.tilemapTiledJSON('map', tilemapJson);
 	}
 
+	generateNoise(density: number) {
+		var rand = Math.floor((Math.random() * 100) + 1);
+		if (density < rand)
+			return 0;
+		else
+			return 1;
+	}
+
+	getSurroundingWallCount(gridX: number, gridY: number, grid: number[][], stateCell: number) {
+
+		let wallCount: number = 0;
+
+		for (let neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++) {
+			for (let neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++) {
+				if (neighbourX >= 0 && neighbourY >= 0 && neighbourX < grid.length && neighbourY < grid[0].length) {
+					if (neighbourX != gridX && neighbourY != gridY) {
+						if (grid[neighbourX][neighbourY] == stateCell) {
+							wallCount++;
+						}
+					}
+				} else {
+					wallCount++;
+				}
+			}
+		}
+
+		return wallCount;
+	}
+
+	applyCellularAutomation(grid: number[][]) {
+
+		let buffer_grid: number[][] = [];
+		for (let i: number = 0; i < grid.length; i++) {
+			buffer_grid[i] = [];
+			for (let j: number = 0; j < grid[i].length; j++) {
+				buffer_grid[i][j] = grid[i][j];
+			}
+		}
+
+		for (let i = 0; i < grid.length; i++) {
+			for (let j = 0; j < grid[i].length; j++) {
+				var neighbours = this.getSurroundingWallCount(i, j, grid, 1);
+				if (neighbours > 4 && buffer_grid[i][j] == 0) {
+					buffer_grid[i][j] = 1;
+				} else if (neighbours < 3 && buffer_grid[i][j] == 1) {
+					buffer_grid[i][j] = 0;
+				}
+			}
+		}
+
+		return buffer_grid;
+	}
+
+	dilate(grid: number[][]) {
+		let buffer_grid: number[][] = [];
+		for (let i: number = 0; i < grid.length; i++) {
+			buffer_grid[i] = [];
+			for (let j: number = 0; j < grid[i].length; j++) {
+				buffer_grid[i][j] = grid[i][j];
+			}
+		}
+
+		//Написать дилатацию
+		for (var i = 0; i < grid.length; i++) {
+			for (var j = 0; j < grid[i].length; j++) {
+				if (grid[i][j] == 1) {
+					for (let neighbourX = i - 1; neighbourX <= i + 1; neighbourX++) {
+						for (let neighbourY = j - 1; neighbourY <= j + 1; neighbourY++) {
+							if (neighbourX >= 0 && neighbourY >= 0 && neighbourX < grid.length && neighbourY < grid[0].length) {
+								buffer_grid[neighbourX][neighbourY] = 1;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return buffer_grid;
+	}
+
+	erode(gridX: number, griY: number) {
+		//Написать эрозию
+	}
+
 	create() {
-		const map = this.make.tilemap({ key: 'map' });
+		var array: number[][] = [];
+		for (var i: number = 0; i < 50; i++) {
+			array[i] = [];
+			for (var j: number = 0; j < 50; j++) {
+				array[i][j] = 0;
+			}
+		}
 
-		const tileset = map.addTilesetImage('Green_Meadow_Tileset', 'tiles');
+		const map = this.make.tilemap({ data: array });
 
-		const layers = createLayers(map, tileset, layersSettings);
-		const collidesLayers = Object.entries<LayerDescription>(layersSettings)
-			.filter(([, { collide: astar }]) => astar)
-			.map(([key]) => key as keyof typeof layersSettings);
-		setupFinder(this.finder, map, collidesLayers);
+		const tileset = map.addTilesetImage('Green_Meadow_Tileset', 'tiles', 32, 32, 2, 6);
+		//const layers = createLayers(map, tileset, layersSettings);
+		const layer = map.createBlankLayer('floor', tileset);
 
 		this.width = map.widthInPixels;
 		this.height = map.heightInPixels;
@@ -111,85 +139,71 @@ export class GameScene extends Phaser.Scene implements Scene {
 		this.physics.world.bounds.width = map.widthInPixels;
 		this.physics.world.bounds.height = map.heightInPixels;
 
+		var things: number[][] = [];
+
+		for (var i: number = 0; i < map.width; i++) {
+			things[i] = [];
+			for (var j: number = 0; j < map.height; j++) {
+				if (i == 0 || i == map.width - 1 || j == 0 || j == map.height - 1) {
+					things[i][j] = 1;
+				} else
+					things[i][j] = this.generateNoise(60);
+			}
+		}
+
+
+		layer.putTilesAt(things, 0, 0);
+
+
 		// Creating characters
 		const characterFactory = new CharacterFactory(this);
 		this.characterFactory = characterFactory;
 
-		// Создаем глобального арбитра
-		const arbitrator = new Arbitrator();
-		const corralArbitratorTile = layers['Corral.Arbitrator'].findTile(
-			(tile: Phaser.Tilemaps.Tile) => tile.index > 0
-		);
-		const arbitratorTiles = layers.Arbitrator.filterTiles(
-			(tile: Phaser.Tilemaps.Tile) => tile.index > 0
-		);
-		characterFactory.buildInnerArbitrator(
-			{ x: corralArbitratorTile.x - 2, y: corralArbitratorTile.y },
-			arbitrator
-		);
-		arbitratorTiles.forEach(a =>
-			characterFactory.buildOuterArbitrator({ x: a.x - 2, y: a.y }, arbitrator)
-		);
-
 		characterFactory.buildPlayerCharacter('aurora', 800, 300);
-		const fence = characterFactory.buildFence(
-			layers['Corral.Doors'],
-			1736,
-			1222
-		);
 
-		const positionCorral = Vector.create(1012, 225);
-		const sizeCorral = Vector.create(225, 150);
+		this.input.keyboard.on('keydown-O', () => {
 
-		characterFactory.buildCorral(positionCorral, sizeCorral, fence);
-		// Создаем желешек
-		const params: BuildSlimeOptions = { slimeType: 0 };
-		for (let i = 0; i < 20; i++) {
-			const x = Phaser.Math.RND.between(
-				50,
-				this.physics.world.bounds.width - 50
-			);
-			const y = Phaser.Math.RND.between(
-				50,
-				this.physics.world.bounds.height - 50
-			);
-			params.slimeType = Phaser.Math.RND.between(0, 4);
-			characterFactory.buildSlime(x, y, params);
-		}
+			things = this.applyCellularAutomation(things);
+			console.log(things);
+			layer.putTilesAt(things, 0, 0);
 
-		characterFactory.buildPunk(100, 200);
-		characterFactory.buildPunk(200, 350);
-		this.input.keyboard.once('keydown-P', () => {
-			// Turn on physics debugging to show player's hitbox
-			this.physics.world.createDebugGraphic();
-
-			this.add.graphics().setAlpha(0.75).setDepth(20);
 		});
-		// Устанавливаем коллизии с окружением для игрока и npc
-		collidesLayers.forEach(layerID => {
-			layers[layerID].setCollisionBetween(1, 5000);
-			this.physics.add.collider(characterFactory.dynamicGroup, layers[layerID]);
+
+		this.input.keyboard.on('keydown-Z', () => {
+			layer.replaceByIndex(0, 5);
+			layer.replaceByIndex(1, -1);
 		});
-		// this.physics.add.collider(
-		// characterFactory.dynamicGroup,
-		// characterFactory.dynamicGroup
-		// );
+		this.input.keyboard.on('keydown-X', () => {
+			layer.replaceByIndex(5, 0);
+			layer.replaceByIndex(-1, 1);
+		});
+
+		this.input.keyboard.on('keydown-D', () => {
+			things = this.dilate(things);
+			layer.putTilesAt(things, 0, 0);
+		});
+		this.input.keyboard.on('keydown-E', () => {
+
+		});
+
+		this.input.keyboard.on('keydown-R', () => {
+			let density = Math.floor((Math.random() * 100) + 1);
+			for (var i: number = 0; i < map.width; i++) {
+				things[i] = [];
+				for (var j: number = 0; j < map.height; j++) {
+					if (i == 0 || i == map.width - 1 || j == 0 || j == map.height - 1) {
+						things[i][j] = 1;
+					} else
+						things[i][j] = this.generateNoise(density);
+				}
+			}
+			layer.putTilesAt(things, 0, 0);
+		});
 	}
 
 	update() {
 		if (this.characterFactory) {
-			if (
-				this.characterFactory.currentSlimesCount <=
-				this.characterFactory.slimeMax * 0.5
-			) {
-				this.scene.start('LoseScene');
-			}
-			const inCorral = this.characterFactory.slimes.filter(slime =>
-				slime.isInCorral()
-			).length;
-			if (inCorral === this.characterFactory.currentSlimesCount) {
-				this.scene.start('WinScene');
-			}
+
 			this.characterFactory.gameObjects.forEach(function (element) {
 				element.update();
 			});
